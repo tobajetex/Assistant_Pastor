3ade92ba81937eb2f8e4656584b1f0bda271966d
Jetawo Oloruntoba <jetawotobajetex@gmail.com>
2026-05-03 01:13:08 +0100

    update:checkEnvironment file was updated to point to only the API_KEY

--- Backend/checkEnvironment.js
+++ Backend/checkEnvironment.js
@@ -1,11 +1,7 @@
 export function checkEnvironment() {
-  const requiredVars = ["AI_URL", "AI_MODEL", "AI_KEY"];
-
-  requiredVars.forEach((key) => {
-    if (!process.env[key]) {
-      throw new Error(`Missing ${key} in .env file`);
-    }
-  });
+  if (!process.env.OPENROUTER_API_KEY) {
+    throw new Error(`Missing API_KEY in .env file`);
+  }
 
   //console.log("✅ Environment variables loaded successfully");
 }
